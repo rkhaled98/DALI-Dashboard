b648e40fdab94cdec24b0aad19efec17750d014b
Rafi Khaled <rkhaled@vassar.edu>
2019-02-02 18:34:48 -0500

    add images to template

--- client/main.js
+++ client/main.js
@@ -6,7 +6,7 @@ import { Session } from 'meteor/session'
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.main.onCreated(function helloOnCreated() {
   // counter starts at 0
   console.log("hello")
   this.counter = new ReactiveVar(0);
@@ -15,34 +15,30 @@ Template.hello.onCreated(function helloOnCreated() {
       console.log(error)
     }
     console.log(result);
-    Session.set('result',result)
+    Session.set('peopleJSON',result)
   });
 });
 
-Template.hello.helpers({
+Template.main.helpers({
   counter() {
     return Template.instance().counter.get();
   },
 
   people(){
-    result = Session.get('result')
-    to_return = []
+    result = Session.get('peopleJSON');
+    people = [];
     for (var member_number in result['data']){
-        to_return.push((result['data'][member_number]['name']));
+        datum = []
+        datum.push((result['data'][member_number]['name']));
+        datum.push("http://mappy.dali.dartmouth.edu/"+(result['data'][member_number]['iconUrl']));
+        people.push(datum)
     }
-    return to_return;
-    // Meteor.call("GET_DATA", "None", (result) => {
-    //   return result;
-    // })
-
-
-    // const result = Meteor.call('getJSON', "None");
-    // return result
+    return people;
 
     },
 });
 
-Template.hello.events({
+Template.main.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     console.log("hello")
