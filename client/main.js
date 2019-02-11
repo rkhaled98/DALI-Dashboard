9a95d18efecd2309dd46f797dd1900d1a8f68d79
Rafi Khaled <rkhaled@vassar.edu>
2019-02-10 19:31:23 -0500

    some cleanup of comments

--- client/main.js
+++ client/main.js
@@ -10,12 +10,10 @@ import './main.html';
 
 Meteor.startup(function () {
   GoogleMaps.load({ v: '3', key: 'AIzaSyAXfPpJ9yoNV03vijE6LAxntmiSN-dtxL4', libraries: 'geometry,places' });
-  // Markers.remove({});
 
   console.log("hello")
 
   Session.set("dataChanged","1")
-  // Session.set("mainmodaltimeout",1200)
   
 
 });
@@ -34,45 +32,6 @@ Template.body.onRendered(function () {
       });
     }
   });
-  // $.getJSON(settings, function(data) {
-  //   return new Promise((resolves, rejects) => {
-  //     data['particles']['shape']['img1'] = 
-  //     {
-  //       "src": "https://scontent.fzty2-1.fna.fbcdn.net/v/t1.0-9/18118884_105583170010283_3402265634081103440_n.jpg?_nc_cat=102&_nc_ht=scontent.fzty2-1.fna&oh=13a5a347847ebab48d43ba3006df1e69&oe=5CB2EB51",
-  //       "width": 100,
-  //       "height": 100,
-  //     };
-  //     // data['particles']['shape']['type'].push('img1');
-  //     data['particles']['shape']['type'].push('img1');
-  //     Meteor.call("writeJSON", data ,(error,result) => {
-  //       console.log(`settings are ${result}`);
-  //       resolves(result);
-  //     });
-  //     // data['particles']['shape']['type'].d
-  //     // console.log(data['particles']['shape']);
-  //     // fs.writeFile('myjsonfile.json', data, 'utf8', function (){
-  //     // resolves(settings);
-  //     // console.log(`settings are ${settings}`);
-  //     // });
-  //   })  
-  // }).then(settings => {
-  //   console.log(settings);
-  //   this.autorun(() => {
-  //     if (particlesJS) {
-  //       console.log(`loading particles.js config from "${settings}"`)
-  //       // window.particlesJS.load = function(tag_id, path_config_json, callback)
-  //       particlesJS.load('particles-js', 'pjs-settings-server.json', function () {
-  //         console.log('callback - particles.js config loaded');
-  //       });
-  //     }
-  //   });
-  // }
-  // );
-  // for (var i = 0; i < sets.length; i++){
-  //   console.log(sets[i]);
-  // }
-
-
 });
 
 Template.body.onCreated(function bodyOnCreated() {
@@ -170,80 +129,6 @@ Template.main.helpers({
 
   },
 });
-
-
-
-
-// Template.map.helpers({  
-//   mapOptions: function() {
-//     if (GoogleMaps.loaded()) {
-//       return {
-//         center: new google.maps.LatLng(10.798042, 35.350880),
-//         zoom: 8
-//       };
-//     }
-//   }
-// });
-
-// Template.map.onCreated(function() {  
-//   GoogleMaps.ready('map', function(map) {
-//     // google.maps.event.addListener(map.instance, 'click', function(event) {
-//     //   Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
-//     // });
-
-//     result = Session.get('peopleJSON');
-//     people = [];
-//     if (Markers.find().count() != result['data'].length) {
-//       for (var member_number in result['data']){
-//         lati = result['data'][member_number]['lat_long'][0];
-//         longi = result['data'][member_number]['lat_long'][1];
-//         Markers.insert({lat: lati, lng: longi})
-
-//     }
-//     }
-
-
-//     var markers = {};
-
-// Markers.find().observe({  
-//   added: function(document) {
-//     // Create a marker for this document
-//     var marker = new google.maps.Marker({
-//       draggable: true,
-//       animation: google.maps.Animation.DROP,
-//       position: new google.maps.LatLng(document.lat, document.lng),
-//       map: map.instance,
-//       // We store the document _id on the marker in order 
-//       // to update the document within the 'dragend' event below.
-//       id: document._id
-//     });
-
-//     // This listener lets us drag markers on the map and update their corresponding document.
-//     google.maps.event.addListener(marker, 'dragend', function(event) {
-//       Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
-//     });
-
-//     // Store this marker instance within the markers object.
-//     markers[document._id] = marker;
-//   },
-//   changed: function(newDocument, oldDocument) {
-//     markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
-//   },
-//   removed: function(oldDocument) {
-//     // Remove the marker from the map
-//     markers[oldDocument._id].setMap(null);
-
-//     // Clear the event listener
-//     google.maps.event.clearInstanceListeners(
-//       markers[oldDocument._id]);
-
-//     // Remove the reference to this marker instance
-//     delete markers[oldDocument._id];
-//   }
-// });
-
-//   });
-// });
 
 Template.newcarousel.onRendered(function newCarouselOnRendered(){
   this.autorun(() => {
--- server/main.js
+++ server/main.js
@@ -4,13 +4,10 @@ import {GET_DATA} from '../imports/backend/httpgetter'
 var fs = require('fs');
 
 Meteor.startup(() => {
-  // GoogleMaps.load();
-  // Markers.remove({}); // Reset the markers da
 });
 
 Meteor.methods({
   getJSON: () =>{
-    // GET_DATA().then(data=>{return data});
     return new Promise((resolves,rejects)=>{
       GET_DATA().then(output=>{
         resolves(output);
@@ -19,10 +16,6 @@ Meteor.methods({
   },
   writeJSON: (current) => {
     var path = process.env['METEOR_SHELL_DIR'] + '/../../../public';
-    // fs.writeFile(path + "/" + 'pjs-settings-server.json', 'Hello content!', function (err) {
-    //   if (err) throw err;
-    //   console.log('Saved!');
-    // });
     fs.writeFileSync(path + "/" + 'pjs-settings-server.json', JSON.stringify(current))
     return 'pjs-settings-server.json'
   }
