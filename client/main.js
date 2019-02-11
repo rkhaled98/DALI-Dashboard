63b0cf3bdb55a16f04f72afdd4d0d9ed81df786f
Rafi Khaled <rkhaled@vassar.edu>
2019-02-10 22:36:09 -0500

    cleaning html,css,js

--- client/main.js
+++ client/main.js
@@ -11,90 +11,12 @@ import './main.html';
 Meteor.startup(function () {
   GoogleMaps.load({ v: '3', key: 'AIzaSyAXfPpJ9yoNV03vijE6LAxntmiSN-dtxL4', libraries: 'geometry,places' });
 
-  console.log("hello")
 
-  Session.set("dataChanged", "1")
-
-  Session.set("loaded", 0)
+  Session.set("loaded", 0) // we use this variable to track whether carousel has loaded
 
 
 });
 
-// Template.body.onRendered(function(){
-//   (new Promise(function(resolve, reject) {
-//     setTimeout(function() {
-//         console.log("successfully resolving");
-//         let $owl = $('.my-carousel-div');
-//         $owl.owlCarousel({
-//           items: 3,
-//           autoplay: true,
-//           autoplayTimeout: 1500,
-//           autoplayHoverPause: true,
-//           margin: 10,
-//           loop: true,
-//           responsiveClass: true,
-//           responsive: {
-//             0: {
-//               items: 1,
-//               nav: true
-//             },
-//             600: {
-//               items: 3,
-//             },
-//             1000: {
-//               items: 6,
-//             }
-//           }
-//         });
-//         resolve();
-//     }, 2000);
-// })).then(function () {
-//   Session.set("loaded", 1)
-//     console.log("Promise succeeded");
-// }, function () {
-//     console.log("Promise failed");
-// });
-// });
-// Template.body.onRendered(function () {
-//   let settings = 'pjs-settings-test.json';
-//   this.autorun(() => {
-//     let lol = Session.get("test");
-//     console.log("we just auto ran!!")
-//     if (particlesJS) {
-//       console.log(`loading particles.js config from "${settings}"...`)
-//       /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-//       particlesJS.load('particles-js', settings, function () {
-//         fetch(`${settings}`).then(data => {console.log(data)})
-//         console.log('callback - particles.js config loaded');
-//       });
-//     }
-//   });
-// });
-
-// Template.body.onCreated(function bodyOnCreated() {
-
-//   let settings = 'pjs-settings.json';
-//   $.getJSON(settings, function(data) {
-//       data['particles']['shape']['img1'] = 
-//       {
-//         "src": "https://scontent.fzty2-1.fna.fbcdn.net/v/t1.0-9/18118884_105583170010283_3402265634081103440_n.jpg?_nc_cat=102&_nc_ht=scontent.fzty2-1.fna&oh=13a5a347847ebab48d43ba3006df1e69&oe=5CB2EB51",
-//         "width": 100,
-//         "height": 100,
-//       };
-//       console.log("wrote it")
-//       // data['particles']['shape']['type'].push('img1');
-//       data['particles']['shape']['type'].push('img1');
-//       console.log("img1")
-//       Meteor.call("writeJSON", data ,(error,result) => {
-//         console.log(result);
-//       });
-
-//   }).then(function () {
-//     Session.set("test", Session.get("test") + "wow!");
-//   }
-//   );
-
-// });
 
 Template.body.helpers({
   searching() {
@@ -105,29 +27,9 @@ Template.body.helpers({
   }
 });
 
-
-
-function reset_people_var() {
-  result = Session.get('peopleJSON');
-  console.log(result);
-  people = [];
-  for (var member_number in result['data']) {
-    datum = []
-    datum.push(member_number);
-    datum.push((result['data'][member_number]['name']));
-    datum.push("http://mappy.dali.dartmouth.edu/" + (result['data'][member_number]['iconUrl']));
-    datum.push((result['data'][member_number]['message']));
-    people.push(datum)
-  }
-  Session.set('peopleArray', people)
-  Session.set('dataChanged', Session.get('dataChanged') + "1")
-  console.log(Session.get('dataChanged'));
-}
-
 Template.main.onCreated(function helloOnCreated() {
   // counter starts at 0
 
-  this.state = new ReactiveDict();
   Meteor.subscribe('markers');
 
   // GET LIVE JSON DATA FROM THE DALI WEBSITE
@@ -170,61 +72,6 @@ Template.main.helpers({
   },
 });
 
-// Template.newcarousel.onRendered(function newCarouselOnRendered(){
-//   this.autorun(() => {
-//     console.log("we just autoran!!!")
-//     let test = Session.get("dataChanged")
-//     setTimeout(function func() {
-//       let $owl = $('.my-carousel-div');
-//       $owl.owlCarousel({
-//         items: 6,
-//         // autoplay: true,
-//         autoplayTimeout: 1500,
-//         autoplayHoverPause: true,
-//         margin: 10,
-//         loop: true,
-//         responsiveClass: true,
-//         responsive: {
-//           0: {
-//             items: 6,
-//             nav: true
-//           },
-//           600: {
-//             items: 6,
-//           },
-//           1000: {
-//             items: 6,
-//           }
-//         }
-//       });
-//     }, 0)
-//   });
-// });
-
-// Template.newcarousel.helpers({
-//   people() {
-//     return Session.get("peopleArrayNew")
-//   }
-// });
-
-// Template.newcarousel.events({
-//   'click div'(event, instance) {
-//     console.log("this worked")
-//     index = event.target.parentElement.id // depends on the HTML in carousel template
-//     console.log(`index is ${index} with type ${typeof(index)}`);
-//     Session.set("test", Session.get("test") + "wow!")
-//     if (index != ""){
-//       result = Session.get('peopleJSON')['data'][index]
-//       console.log(result)
-//       Session.set("person_to_show_modal", result)
-//       Modal.show('ModalPopup')
-//     }
-//   // Session.set("test", Session.get("test") + "wow!");
-
-//   },
-// });
-
-
 Template.carousel.onRendered(function carouselOnRendered() {
 
   (new Promise(function(resolve, reject) {
@@ -256,43 +103,8 @@ Template.carousel.onRendered(function carouselOnRendered() {
     }, 2750);
 })).then(function () {
   Session.set("loaded", 1)
-    console.log("Promise succeeded");
-}, function () {
-    console.log("Promise failed");
 });
-  // let test = Session.get("dataChanged")
-  // setTimeout(function func() {
-  //   let $owl = $('.my-carousel-div');
-  //   $owl.owlCarousel({
-  //     items: 3,
-  //     autoplay: true,
-  //     autoplayTimeout: 1500,
-  //     autoplayHoverPause: true,
-  //     margin: 10,
-  //     loop: true,
-  //     responsiveClass: true,
-  //     responsive: {
-  //       0: {
-  //         items: 1,
-  //         nav: true
-  //       },
-  //       600: {
-  //         items: 3,
-  //       },
-  //       1000: {
-  //         items: 6,
-  //       }
-  //     }
-  //   });
-  // }, 2000).then(() => {
-  //   console.log("LOADED!")
-  //   Session.set("loaded", 1)
-  // })
-  // });
-
-
-}
-);
+});
 
 Template.carousel.events({
   'click div'(event, instance) {
@@ -442,3 +254,169 @@ Template.searchedPeople.events({
     }
   }
 });
+
+
+
+// Template.newcarousel.onRendered(function newCarouselOnRendered(){
+//   this.autorun(() => {
+//     console.log("we just autoran!!!")
+//     let test = Session.get("dataChanged")
+//     setTimeout(function func() {
+//       let $owl = $('.my-carousel-div');
+//       $owl.owlCarousel({
+//         items: 6,
+//         // autoplay: true,
+//         autoplayTimeout: 1500,
+//         autoplayHoverPause: true,
+//         margin: 10,
+//         loop: true,
+//         responsiveClass: true,
+//         responsive: {
+//           0: {
+//             items: 6,
+//             nav: true
+//           },
+//           600: {
+//             items: 6,
+//           },
+//           1000: {
+//             items: 6,
+//           }
+//         }
+//       });
+//     }, 0)
+//   });
+// });
+
+// Template.newcarousel.helpers({
+//   people() {
+//     return Session.get("peopleArrayNew")
+//   }
+// });
+
+// Template.newcarousel.events({
+//   'click div'(event, instance) {
+//     console.log("this worked")
+//     index = event.target.parentElement.id // depends on the HTML in carousel template
+//     console.log(`index is ${index} with type ${typeof(index)}`);
+//     Session.set("test", Session.get("test") + "wow!")
+//     if (index != ""){
+//       result = Session.get('peopleJSON')['data'][index]
+//       console.log(result)
+//       Session.set("person_to_show_modal", result)
+//       Modal.show('ModalPopup')
+//     }
+//   // Session.set("test", Session.get("test") + "wow!");
+
+//   },
+// });
+
+
+  // let test = Session.get("dataChanged")
+  // setTimeout(function func() {
+  //   let $owl = $('.my-carousel-div');
+  //   $owl.owlCarousel({
+  //     items: 3,
+  //     autoplay: true,
+  //     autoplayTimeout: 1500,
+  //     autoplayHoverPause: true,
+  //     margin: 10,
+  //     loop: true,
+  //     responsiveClass: true,
+  //     responsive: {
+  //       0: {
+  //         items: 1,
+  //         nav: true
+  //       },
+  //       600: {
+  //         items: 3,
+  //       },
+  //       1000: {
+  //         items: 6,
+  //       }
+  //     }
+  //   });
+  // }, 2000).then(() => {
+  //   console.log("LOADED!")
+  //   Session.set("loaded", 1)
+  // })
+  // });
+
+
+
+// Template.body.onRendered(function(){
+//   (new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//         console.log("successfully resolving");
+//         let $owl = $('.my-carousel-div');
+//         $owl.owlCarousel({
+//           items: 3,
+//           autoplay: true,
+//           autoplayTimeout: 1500,
+//           autoplayHoverPause: true,
+//           margin: 10,
+//           loop: true,
+//           responsiveClass: true,
+//           responsive: {
+//             0: {
+//               items: 1,
+//               nav: true
+//             },
+//             600: {
+//               items: 3,
+//             },
+//             1000: {
+//               items: 6,
+//             }
+//           }
+//         });
+//         resolve();
+//     }, 2000);
+// })).then(function () {
+//   Session.set("loaded", 1)
+//     console.log("Promise succeeded");
+// }, function () {
+//     console.log("Promise failed");
+// });
+// });
+// Template.body.onRendered(function () {
+//   let settings = 'pjs-settings-test.json';
+//   this.autorun(() => {
+//     let lol = Session.get("test");
+//     console.log("we just auto ran!!")
+//     if (particlesJS) {
+//       console.log(`loading particles.js config from "${settings}"...`)
+//       /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+//       particlesJS.load('particles-js', settings, function () {
+//         fetch(`${settings}`).then(data => {console.log(data)})
+//         console.log('callback - particles.js config loaded');
+//       });
+//     }
+//   });
+// });
+
+// Template.body.onCreated(function bodyOnCreated() {
+
+//   let settings = 'pjs-settings.json';
+//   $.getJSON(settings, function(data) {
+//       data['particles']['shape']['img1'] = 
+//       {
+//         "src": "https://scontent.fzty2-1.fna.fbcdn.net/v/t1.0-9/18118884_105583170010283_3402265634081103440_n.jpg?_nc_cat=102&_nc_ht=scontent.fzty2-1.fna&oh=13a5a347847ebab48d43ba3006df1e69&oe=5CB2EB51",
+//         "width": 100,
+//         "height": 100,
+//       };
+//       console.log("wrote it")
+//       // data['particles']['shape']['type'].push('img1');
+//       data['particles']['shape']['type'].push('img1');
+//       console.log("img1")
+//       Meteor.call("writeJSON", data ,(error,result) => {
+//         console.log(result);
+//       });
+
+//   }).then(function () {
+//     Session.set("test", Session.get("test") + "wow!");
+//   }
+//   );
+
+// });
+
